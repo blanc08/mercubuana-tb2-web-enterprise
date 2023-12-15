1dde9837d351d27c83c006bf69d42daa3b51fe84
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-12-15 23:49:57 +0700

    [bug fix] revert supabase config

--- src/utils/supabase/index.js
+++ src/utils/supabase/index.js
@@ -1,6 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(process.env.
-    NEXT_PUBLIC_SUPABASE_URL = "https://lajqkxyrpsqhgchdloij.supabase.co", process.env.
-    NEXT_PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhanFreHlycHNxaGdjaGRsb2lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3MjA4MjIsImV4cCI6MjAxNDI5NjgyMn0.PM08n6GKVZoWsC3z1MVc1VRuZBxsrSCr1624YMHqyno"
-    );
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
