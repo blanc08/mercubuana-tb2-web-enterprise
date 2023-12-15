bf7a68105c2184f0131d9b0d08e385a69d42f900
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-12-16 01:25:57 +0700

    [update] booking page dan rubah navbar

--- src/app/booking/[id]/page.js
+++ src/app/booking/[id]/page.js
@@ -1,153 +1,138 @@
 "use client";
 
 import { supabase } from "@/utils/supabase";
-import { HistoryOutlined, UserOutlined } from "@ant-design/icons";
-import { Avatar, Button, Divider } from "antd";
-import { Typography } from "antd";
-import Image from "next/image";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const { Title, Text } = Typography;
-
 export default function Page() {
-  const [histories, setHistories] = useState([]);
+  const [order, setOrder] = useState({});
+  const params = useParams();
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const { data } = await supabase.from("orders").select(
-      `*, 
+    const id = params["id"];
+    const { data } = await supabase
+      .from("orders")
+      .select(
+        `*, 
       users(*),
       tickets(*)
       `
-    );
+      )
+      .eq("id", id);
 
-    setHistories(data);
+    // console.log(data[0]);
+
+    setOrder(data[0]);
   };
 
+  const handlePayment = () => {};
+
+  if (!order || !order.id) return <>loading...</>;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-ghost-white text-raisin-black">
-      <div className="grid min-w-[780px] grid-cols-3 gap-4">
-        {/* left */}
-        <div className="col-span-1 border-gray-200 border-2 p-4 rounded">
-          <div id="header" className="flex flex-row gap-2">
-            <Avatar size="large" icon={<UserOutlined />} />
-
-            <div className="">
-              <Title level={4} style={{ marginBottom: 0 }}>
-                Nama User
-              </Title>
-              <Text type="secondary" style={{ fontSize: 13 }}>
-                google
-              </Text>
+    <main className="flex flex-col px-4 py-8 bg-ghost-white min-h-screen text-raisin-black ">
+      {/* card */}
+      <div className="bg-white rounded-lg px-4 py-3 mx-auto">
+        <div className="flex flex-row gap-4">
+          <div className="flex items-center mb-6">
+            <div className="ml-4">
+              <p className="text-xl font-semibold">
+                Review & Pembayaran <span className="text-lg font-semibold">Tripmu ke {order.tickets?.arrival}</span>
+              </p>
+              <p className="text-gray-500">Hampir selesai! Double-check semuanya sebelum konfirmasi bookingmu.</p>
             </div>
           </div>
-          <Divider />
 
-          <div>
-            <Button type="link" icon={<HistoryOutlined />}>
-              My Booking
-            </Button>
+          <div className="flex flex-col space-y-6 mb-8">
+            <div className="border rounded-lg px-4 py-2">
+              <p className="text-lg font-semibold">Flight (GA-789)</p>
+              <ul className="text-gray-500 mt-2">
+                <li>
+                  {order.tickets.departure} ({order.tickets.departure_airport}) - {order.tickets.arrival} (
+                  {order.tickets.arrival_airport})
+                </li>
+                <li>Departure : {new Date(order.tickets?.departure_at).toLocaleString()}</li>
+                <li>Arrive : {new Date(order.tickets?.arrival_estimation_at).toLocaleDateString()}</li>
+                {/* <li>1 hour 30 minutes</li> */}
+              </ul>
+            </div>
+
+            <div className="border rounded-lg px-4 py-2">
+              <p className="text-lg font-semibold">Informasi lainnya</p>
+              <ul className="text-gray-500 mt-2">
+                <li>Bagasi : {order.tickets?.baggage}</li>
+                <li>Kabin : {order.tickets?.cabin}</li>
+              </ul>
+            </div>
           </div>
         </div>
 
-        {/* right */}
-        <div className="col-span-2 flex flex-col">
-          <div style={{ marginBottom: "32px" }}>
-            <a href="/en-id/reschedule/flight" target="_blank" className="css-4rbku5">
-              <div className="bg-primary w-full px-10 py-8 rounded-lg shadow-lg text-ghost-white grid grid-cols-2 overflow-hidden">
-                <Image
-                  importance="low"
-                  loading="lazy"
-                  src="/images/1523941664489-1f98d3a1485460f5a9ba7ced54fa97ac.webp"
-                  alt="gambar tambahan"
-                  decoding="async"
-                  width="160"
-                  className="object-fill col-span-1 items-center my-auto"
-                  height={100}
-                />
-                <div dir="auto" className="flex-wrap whitespace-pre-wrap max-w-xs order-last" id="rescheduleBannerText">
-                  <h3 className="text-lg mb-5 font-semibold">Traveloka Easy Reschedule</h3>
-                  <p className="text-gray-50 text-sm">
-                    Changes are unavoidable. Make this one easy. <span className="font-bold">Learn more</span>
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
+        {/* form pembayaran */}
+        <form>
+          <h2 class="text-2xl font-semibold mb-6">Detail Kontak</h2>
 
-          {/* Tiket Aktif */}
-          <h1 className="text-2xl font-semibold text-gray-700 mb-2">Active E-tickets</h1>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label class="block mb-2" for="fullname">
+                Full Name:
+              </label>
+              <input type="text" id="fullname" name="fullname" class="w-full px-4 py-2 border rounded-md" required />
+            </div>
 
-          {/* card Tiket Aktif */}
-          {histories.length === 0 ? (
-            <div className="bg-white rounded-lg px-4 py-3 flex flex-row gap-4">
-              <Image
-                importance="low"
-                loading="lazy"
-                src="/images/1594367281441-5ec1b573d106b7aec243b19efa02ac56.svg"
-                alt="icon 1"
-                decoding="async"
-                width="96"
-                height="96"
-                className="object-fill"
+            <div>
+              <label class="block mb-2" for="email">
+                Email:
+              </label>
+              <input type="email" id="email" name="email" class="w-full px-4 py-2 border rounded-md" required />
+            </div>
+
+            <div>
+              <label class="block mb-2" for="phone">
+                Phone Number:
+              </label>
+              <input type="tel" id="phone" name="phone" class="w-full px-4 py-2 border rounded-md" required />
+            </div>
+
+            <div>
+              <label class="block mb-2" for="card">
+                Credit Card Number:
+              </label>
+              <input type="text" id="card" name="card" class="w-full px-4 py-2 border rounded-md" required />
+            </div>
+
+            <div>
+              <label class="block mb-2" for="expiration">
+                Expiration Date:
+              </label>
+              <input
+                type="text"
+                id="expiration"
+                name="expiration"
+                class="w-full px-4 py-2 border rounded-md"
+                placeholder="MM/YY"
+                required
               />
-              <div className="p-5">
-                <h3 aria-level="3" dir="auto" role="heading" className="text-gray-800 font-semibold mb-2">
-                  No Active Bookings Found
-                </h3>
-                <div dir="auto" className="text-sm">
-                  Anything you booked shows up here, but it seems like you haven&apos;t made any. Let&apos;s create one
-                  via homepage!
-                </div>
-              </div>
             </div>
-          ) : (
-            histories.map((history) => (
-              <div key={history.id} class="border-2 p-3 rounded">
-                <div class="mb-2">
-                  <div class="flex flex-row items-center">
-                    <Image
-                      src="/images/4e4ae9f6-723c-401f-9575-331bd94db641-1641796637554-b13f668a643fcb5d3596f35d7b0f198b.webp"
-                      width={40}
-                      height={40}
-                      alt="vertical-icon"
-                    />
-                    <div class="font-semibold leading-loose text-lg">
-                      <span class="TiketCard_truncate__MIEmZ TiketCard_text_black__N5pYX HcPVsG_text HcPVsG_size_b1 HcPVsG_weight_bold">
-                        {history.tickets.departure} - {history.tickets.arrival}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div class="ml-4 mb-2">
-                  <span class="font-semibold text-gray-500 opacity-80">Finished</span>
-                </div>
-                <div class="ml-4 text-gray-500">
-                  <div class="font-light">
-                    <div class="">
-                      <span class="">
-                        {new Date(history.tickets.departure_at).toLocaleString()} -{" "}
-                        {new Date(history.tickets.arrival_estimation_at).toLocaleTimeString()}
-                      </span>
-                    </div>
-                    <div class="">
-                      <span class="">
-                        {history.tickets.flight} * {history.tickets.departure_airport} -{" "}
-                        {history.tickets.arrival_airport}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div class="TiketCard_divider_mobile_layout__NoepM">
-                  <div class="TiketCard_bottom_row__L563a"></div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
+
+            <div>
+              <label class="block mb-2" for="cvv">
+                CVV:
+              </label>
+              <input type="text" id="cvv" name="cvv" class="w-full px-4 py-2 border rounded-md" required />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="mt-4 bg-primary text-white px-6 py-2 rounded-md hover:bg-blue-700 mb-2 float-right"
+          >
+            Complete Purchase
+          </button>
+        </form>
       </div>
     </main>
   );
--- src/components/template/navbar.jsx
+++ src/components/template/navbar.jsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 // components/Navbar.js
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   const [menuVisible, setMenuVisible] = useState(false);
@@ -11,54 +11,47 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex items-center justify-between p-4 bg-cinereous text-white">
+    <nav className="flex items-center justify-between p-4">
       <div className="flex items-center">
-        <Link href="/home" className='hover:text-secondary'>
-            Plane Reservations
+        <Link href="/home" className="text-primary font-bol">
+          Plane Reservations
         </Link>
       </div>
       <div className="hidden md:flex space-x-4">
-        <Link href="/orders" className='py-1 hover:text-red-500'>
+        <Link href="/orders" className="py-1 text-primary">
           Pesanan
         </Link>
-        <Link href="/login" className=' hover:bg-blue-900 border border-white rounded px-5 py-1'>
-            <button>Login</button>
-          
+        <Link href="/login" className="border text-gray-700 rounded px-5 py-1">
+          <button>Login</button>
         </Link>
-        <Link href="/register" className='bg-primary rounded px-5 py-1 hover:bg-blue-900 '>
-            <button>Daftar</button>    
+        <Link href="/register" className="bg-primary rounded text-gray-50  px-5 py-1">
+          <button>Daftar</button>
         </Link>
       </div>
       <div className="md:hidden">
         <button onClick={toggleMenu}>
           <svg
-            className="w-6 h-6 text-white cursor-pointer"
+            className="w-6 h-6 cursor-pointer"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
       </div>
       {menuVisible && (
         <div className="md:hidden">
           <div className="flex flex-col space-y-4 mt-4">
-            <Link href="/orders" className='hover:text-red-500'>
+            <Link href="/orders" className="text-primary">
               Pesanan
             </Link>
-            <Link href="/login" className='bg-primary text-center px-3 py-1 rounded hover:text-red-500'>
-                <button className='bg-primary'>Login</button>
-              
+            <Link href="/login" className="bg-primary text-center px-3 py-1 rounded text-primary">
+              <button className="bg-primary">Login</button>
             </Link>
-            <Link href="/register" className='bg-primary text-center px-3 py-1 rounded hover:text-red-500'>
-              <button className='bg-primary'>Daftar</button>
+            <Link href="/register" className="bg-primary text-center px-3 py-1 rounded text-primary">
+              <button className="bg-primary">Daftar</button>
             </Link>
           </div>
         </div>
