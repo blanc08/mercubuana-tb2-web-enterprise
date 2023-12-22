dd77a1fc5367d7412241ec3dc5e2229a4d1d8042
mfaizalsyahroni <121980257+mfaizalsyahroni@users.noreply.github.com>
2023-12-22 20:06:05 +0700

    login and register

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,30 +1,87 @@
+'use client'
+
+import React, { useClient, useEffect, useState } from 'react'
+import { supabase } from "@/utils/supabase";
+import { useRouter } from 'next/navigation';
+
+
+//memanggil supabase
+
 const LoginPage = () => {
+    //define state : format [namaState, fungsiUbahState] = useState(default [])
+    const router = useRouter()
+    const [username, setUname] = useState('');
+    const [password, setPwd] = useState('');
+
+   
+
+    const hdlUName = event => {    
+        setUname(event.target.value);
+    }
+
+    const hdlPwd = event => {
+        setPwd(event.target.value);
+    }
+
+    
+
+
+    //cara akses api via libary supabase
+    async function onSubmit(e) {
+        e.preventDefault()
+            const { data, error } = await supabase
+                .from('users')
+                .select('id')
+                .eq('username', username)
+                .eq('password', password)
+                .single()
+
+
+                if (!data) {
+                return alert ("password salah");   
+                
+                } 
+                
+                
+                router.push("/home");
+
+                //Aray
+
+                //.from(tabel target)
+                //    .insert('field', value)
+
+           
+           
+        //alert("");
+
+
+        //samain kaya login bikin validasi keluarin pesan eror dan kalau berhasil pindahin ke login
+
+        //sembunyikan password
+        //created_at dibuat sesuai jam taken akun
+
+        //setListData(data)
+    }
+
     return (
 
 
         <section class="bg-gray-50 dark:bg-gray-50 flex justify-center py-8 mx-auto md:h-screen lg:py-20">
-           <div>
+            <div>
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-50 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
                             Sign in tou you account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        
+                        <form onSubmit={onSubmit} class="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your username</label>
+                                <input onChange={hdlUName} value={username} type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-                            </div>
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-50 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="terms" class="font-light text-gray-950 dark:text-black">Remember me <a class="font-medium text-primary-600 hover:underline dark:text-sky-500" href="#"> &nbsp; &nbsp; Forgot password</a></label>
-                                </div>
+                                <input onChange={hdlPwd} value={password} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <button type="submit" class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Sign in</button>
                             <p class="text-sm font-light text-gray-900 dark:text-black">
---
+++ src/app/register/page.jsx
@@ -0,0 +1,89 @@
+'use client'
+
+    import React, { useState } from 'react'
+    import { useRouter } from 'next/navigation'
+
+    //memanggil supabase
+    import { supabase } from "@/utils/supabase";
+    
+    const RegisterPage = () => {
+        const router = useRouter()
+
+        const [usernameInput, setUname] = useState('')
+        const [passwordInput, setPwd] = useState('')
+
+
+        const hdlUName = event => {
+            setUname(event.target.value);
+        }
+    
+        const hdlPwd = event => {
+            setPwd(event.target.value);
+        }
+
+
+
+        //insert data ke supabase
+        async function onSubmit(event) {
+            event.preventDefault();
+
+            alert("submit diproses lagi");
+
+            const { data, error } = await supabase
+                .from('users')
+                .insert({ username: usernameInput, password: passwordInput, })
+
+            //daftar pakai username yang sudah ada di supabase    
+                if (error) {
+                    return alert ("password salah");   
+                    } 
+                    
+                
+                    router.push('/login/')
+
+            //.from(tabel target)
+                //    .insert('field', value)
+
+
+        }
+
+        return (
+
+
+            <section className="bg-gray-50 dark:bg-gray-50 flex justify-center py-8 mx-auto md:h-screen lg:py-20">
+                <div>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-50 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
+                                Create and account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your username</label>
+                                    <input onChange={hdlUName} type="text" name="username" id="username" value={usernameInput}  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                </div>
+                                <div>
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                                    <input onChange={hdlPwd} value={passwordInput} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                </div>
+                                <div>
+                                    <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm password</label>
+                                    <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                </div>
+                                <button onClick={onSubmit} type="submit" className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Create an account</button>
+                                <p className="text-sm font-light text-gray-900 dark:text-black">
+                                    Already have an account? <a href="login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+        );
+    };
+
+
+    export default RegisterPage;
